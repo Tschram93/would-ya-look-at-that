a7984b04ad4a300171d674f6c4e8e51f681b3ad0
TS93 <tschram93@yahoo.com>
2021-12-10 19:27:14 -0500

    another approach to styles with .jsx

--- src/component/counter.jsx
+++ src/component/counter.jsx
@@ -5,15 +5,10 @@ class Counter extends React.Component {
         count: 0,
     };
 
-    styles = {
-        fontSize: '16px',
-        fontWeight: 'bold',
-    };
-
 	render() {
 		return (
 			<React.Fragment>
-				<span style={this.styles} className='badge bg-primary m-2'>{this.formatCount()}</span>
+				<span style={{fontSize:'1.75rem'}} className='badge bg-primary m-2'>{this.formatCount()}</span>
 				<button className="btn btn-secondary btn-sm"> Increment </button>
 			</React.Fragment>
 		);
