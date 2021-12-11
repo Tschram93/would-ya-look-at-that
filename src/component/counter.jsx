12104ee96ea0686f8c9c0aa13b2601379fb6c879
TS93 <tschram93@yahoo.com>
2021-12-10 19:22:41 -0500

    Different approaches to using style properties with .jsx

--- src/component/counter.jsx
+++ src/component/counter.jsx
@@ -2,14 +2,19 @@ import React from 'react';
 
 class Counter extends React.Component {
     state= {
-        count: 0
+        count: 0,
+    };
+
+    styles = {
+        fontSize: '16px',
+        fontWeight: 'bold',
     };
 
 	render() {
 		return (
 			<React.Fragment>
-				<span>{this.formatCount()}</span>
-				<button className="btn btn-success"> Increment </button>
+				<span style={this.styles} className='badge bg-primary m-2'>{this.formatCount()}</span>
+				<button className="btn btn-secondary btn-sm"> Increment </button>
 			</React.Fragment>
 		);
 	}
