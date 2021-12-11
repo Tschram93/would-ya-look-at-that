6db0e57f266a1ec00faabcca8a2c1aa24974b7d7
TS93 <tschram93@yahoo.com>
2021-12-10 20:56:16 -0500

    deleted comments

--- src/component/counter.jsx
+++ src/component/counter.jsx
@@ -6,18 +6,15 @@ class Counter extends React.Component {
 		tags: ['tag1', 'tag2', 'tag3'],
 	};
 
-// Binding is very powerful. Remember to use in the future!
-
     handleIncrement = () => {
-        // React uses and requires setState() to update the DOM to what has changed.
         this.setState({ count: this.state.count +1 });
-    }                   // is taking the count state and adjusting it to increase by one each time the function is called.
+    }
 
 	render() {
 		return (
 			<div>
 				<span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-				<button onClick={this.handleIncrement} className="btn btn-secondary btn-sm"> Increment </button>
+				<button onClick={() => this.handleIncrement()} className="btn btn-secondary btn-sm"> Increment </button>
 			</div>
 		);
 	}
@@ -32,19 +29,5 @@ class Counter extends React.Component {
 		return count === 0 ? 'Zero' : count;
 	}
 }
-
-// renderTags() {
-//     if (this.state.tags.length === 0)
-//         return <p>There are currently no tags.</p>;
-
-//     return (
-//         <ul>
-//             {this.state.tags.map((tag) => (
-//                 <li key={tag}>{tag}</li>
-//             ))}
-//         </ul>
-//     );
-// }
-// {this.renderTags()}
 
 export default Counter;
