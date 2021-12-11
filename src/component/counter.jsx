6ba424930a00f16667d9bb18e0c21f29cc56217c
TS93 <tschram93@yahoo.com>
2021-12-10 19:52:05 -0500

    list element is created and using the state.tag to input into the list using map func

--- src/component/counter.jsx
+++ src/component/counter.jsx
@@ -3,14 +3,18 @@ import React from 'react';
 class Counter extends React.Component {
     state= {
         count: 0,
+        tags: ['tag1', 'tag2', 'tag3']
     };
 
 	render() {
 		return (
-			<React.Fragment>
+			<div>
 				<span className={this.getBadgeClasses()}>{this.formatCount()}</span>
 				<button className="btn btn-secondary btn-sm"> Increment </button>
-			</React.Fragment>
+                <ul>
+                    { this.state.tags.map( tag => <li key={tag}>{ tag }</li>) }
+                </ul>
+			</div>
 		);
 	}
     getBadgeClasses() {
